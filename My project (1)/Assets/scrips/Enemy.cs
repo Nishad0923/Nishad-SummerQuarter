ae3b6bbd88248f9c013ee30ef5d510d2b4a406c3
Nishad0923 <nishad.thomas@hgs.hiddengeniusproject.org>
2026-07-13 15:07:13 -0700

    working enemy
    getting enemy working

--- My project (1)/Assets/scrips/Enemy.cs
+++ My project (1)/Assets/scrips/Enemy.cs
@@ -1,24 +1,45 @@
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UI;
 public class Enemy : MonoBehaviour
 {
-    [SerializeField] private Transform target; // Stores the target of this enemy
+    
+    [SerializeField] private Transform[] waypoints; // Stores the target of this enemy
     [SerializeField] private NavMeshAgent agent; // Stores the NavMeshAgent component attached to this enemy
+    [SerializeField] private int currentWaypoint = 0; // Internal tracker for what waypoint the agent is following
 
        private void Awake()
     {
         // Get the NavMeshAgent component attached to this enemy
         agent = GetComponent<NavMeshAgent>();
     }
+
+    private void Start()
+    {
+        // Check if the array of waypoints is not empty, then set the first destination
+        if (waypoints.Length > 0)
+        {
+            agent.SetDestination(waypoints[0].position);
+        }
+    }
         
      private void Update()
     {
-        // Check if the target to follow
-        if (target != null)
+        if (waypoints.Length == 0) return; // Do nothing if there are no waypoints left
+
+        // if the agent has reached their current waypoint...
+        if (!agent.pathPending && agent.remainingDistance <= agent.stoppingDistance)
         {
-            // Follow the target
-            agent.SetDestination(target.position);
-        // This function is caled when this enemy collides with another object
+            // Set the next waypoint
+            currentWaypoint++;
+
+            if (currentWaypoint >= waypoints.Length)
+            {
+                currentWaypoint = waypoints.Length - 1;
+                // Or loop back to 0 if desired.
+            }
+
+            agent.SetDestination(waypoints[currentWaypoint].position);
         }
     }
     private void OnCollisionEnter(Collision collision)
